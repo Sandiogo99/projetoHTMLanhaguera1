38236efa583b6f34101d837308bc17e1e20aed5b
Sandiogo99 <diogo.lima25l@gmail.com>
2026-09-09 20:35:10 -0300

    terceiro

--- JS/funcoes.js
+++ JS/funcoes.js
@@ -1,11 +1,43 @@
 function somar() {
-    let num1 = document.getElemenByid("num1").value;
-    let num2 = document.getElemenByid("num1").value;
-    let resultado = parsefloat(num1) + parsefloat(num2);
-    localstore.setItem("resultado",resultado);
-    document.getElemenByid("resultado").value = resultado;
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let resultado = parseFloat(num1) + parseFloat(num2);
+    localStorage.setItem('resultado',resultado);
+    document.getElementById('resultado').value = resultado;
 }
 
+function subtrair(){
+    let nun1 = document.getElementById('num1').value;
+    let nun2 = document.getElementById('num2').value;
+    let resultado = parseFloat(nun1) - parseFloat(nun2);
+
+    localStorage.setItem('resultado', resultado);
+    document.getElementById('resultado').value = resultado; 
+}
+
+function multiplicar(){
+    let nun1 = document.getElementById('num1').value;
+    let nun2 = document.getElementById('num2').value;
+    let resultado = parseFloat(nun1) * parseFloat(nun2);
+    localStorage.setItem('resultado',resultado);
+    document.getElementById('resultado').value = resultado;
+}
+
+function dividir(){
+    let nun1 = document.getElementById('num1').value;
+    let nun2 = document.getElementById('num2').value;
+    let resultado = 0;
+    if (nun2<= 0){
+        alert("nao pode dividir por zero ")
+        document.getElementById('num2').value = 1;
+    } else{
+        resultado = parseFloat(nun1) / parseFloat(nun2);
+    }
+    localStorage.setItem('resultado',resultado);
+    document.getElementById('resultado').value = resultado;
+}
+ 
 window.onload = function(){
-    document.getElemenByid("resultado").value = localstorege.getItem("resultado");
+    document.getElementById('resultado').value = localStorage.getItem('resultado');
+    
 }
